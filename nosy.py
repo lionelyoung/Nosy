aa23c783b27169e8287b9ca939a5158ac6a674de
Lionel Young <lionelyoung@gmail.com>
2010-01-23 10:45:07 -0800

    Modified to be used as an executable with default arguments.
    * Current default args are: '-vv'
    * Added an 'if __name__' section

--- nosy.py
+++ nosy.py
@@ -1,32 +1,35 @@
+#!/usr/bin/env python
 # By Jeff Winkler, http://jeffwinkler.net
+# Forked from wkral http://github.com/wkral/Nosy
 
 import os,stat,time,sys,fnmatch
 
-
 EXTENSIONS = ['*.py']
 EXECUTABLE = 'nosetests'
+DEFAULTARGS = '-vv'
 
 '''
 Watch for changes in all file types specified in 'EXTENSIONS'. 
-If changes, run test executable in 'EXECUTABLE'. 
+If changes, run test executable in 'EXECUTABLE', with default
+arguments 'DEFAULTARGS'. 
 '''
 def checkSum():
     ''' Return a long which can be used to know if any .py files have changed.'''
     val = 0
     for root, dirs, files in os.walk(os.getcwd()):
-	for extension in EXTENSIONS:
+        for extension in EXTENSIONS:
             for f in fnmatch.filter(files, extension):
                 stats = os.stat (os.path.join(root, f))
                 val += stats [stat.ST_SIZE] + stats [stat.ST_MTIME]
     return val
 
-val=0
-try:
-    while (True):
-        if checkSum() != val:
-            val=checkSum()
-            os.system ('%s %s' % (EXECUTABLE, ' '.join(sys.argv[1:])))
-        time.sleep(1)
-except KeyboardInterrupt:
-    print 'Goodbye'
-
+if __name__ == '__main__':
+    val=0
+    try:
+        while (True):
+            if checkSum() != val:
+                val=checkSum()
+                os.system ('%s %s %s' % (EXECUTABLE, DEFAULTARGS, ' '.join(sys.argv[1:])))
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print 'Goodbye'
