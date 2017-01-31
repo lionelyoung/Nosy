a44f85a0e69c6ef57adc829ef91a985befedc229
ly <ly>
2017-01-31 11:48:09 +0800

    Merge latest config changes from wkral

--- nosy.py
+++ nosy.py
@@ -1,51 +1,89 @@
-#!/usr/bin/env python
+# By Jeff Winkler, http://jeffwinkler.net
+# Enhanced by William Kral
 
-"""
-A simple testrunner for nose (or anything else).
-
-Watch for changes in all file types specified in 'EXTENSIONS'.
-If changes, run test executable in 'EXECUTABLE', with default
-arguments 'DEFAULTARGS'.
-
-Originally by Jeff Winkler, http://jeffwinkler.net
-Forked from wkral http://github.com/wkral/Nosy
-"""
-
+import fnmatch
+import json
 import os
 import stat
-import time
-import datetime
+import subprocess
 import sys
-import fnmatch
+import time
+
+'''
+Nosy
+====
+
+Nosy will watch your project's source folder and execute a command of your
+choosing whenever a watched source file changes.
 
 
-EXTENSIONS = ['*.py']
-EXECUTABLE = 'nosetests'
-DEFAULTARGS = '-vv'
+Config file precedence:
+-----------------------
+
+    `pwd`/.nosy
+    ~/.nosy
+
+JSON Config example:
+--------------------
+
+    { "exec": "nosetests",
+      "watch": ["*.py", "*.html", "*.js"] }
+'''
 
 
-def checkSum():
-    """
-    Return a long which can be used to know if any .py files have changed.
-    """
+def load_config_file(config_path):
+    try:
+        if os.path.exists(config_path):
+            with open(config_path, 'r') as f:
+                return json.load(f)
+    except:
+        sys.stderr.write("Error parsing json config files.\n")
+        sys.exit(1)
+
+    return {}
+
+
+def load_config():
+    ''' Load the configuration data based on the current working directory or
+        the home folders default configurations '''
+
+    config = {'exec': 'nosetests', 'watch': ['*.py']}
+
+    config.update(load_config_file(os.path.expanduser("~/.nosy")))
+    config.update(load_config_file('.nosy'))
+
+    return config
+
+
+def check_sum(config):
+    ''' Stat all files matching the patterns in config['watch'] '''
     val = 0
     for root, dirs, files in os.walk(os.getcwd()):
-        for extension in EXTENSIONS:
+        for extension in config['watch']:
             for f in fnmatch.filter(files, extension):
                 stats = os.stat(os.path.join(root, f))
                 val += stats[stat.ST_SIZE] + stats[stat.ST_MTIME]
     return val
 
-if __name__ == '__main__':
-    val = 0
+
+def main():
+    ''' Continuously run check_sum on path expressions in the config
+        If there is a change in any of those files execute the test runner'''
+    config = load_config()
+
+    args = ' '.join(sys.argv[1:])
+    command = '{0} {1}'.format(config['exec'], args)
+
+    last_sum = 0
     try:
-        while True:
-            if checkSum() != val:
-                val = checkSum()
-                os.system('%s %s %s' % (EXECUTABLE, DEFAULTARGS,
-                    ' '.join(sys.argv[1:])))
-                print datetime.datetime.now().__str__()
-                print '=' * 77
+        while (True):
+            new_sum = check_sum(config)
+            if new_sum != last_sum:
+                last_sum = new_sum
+                subprocess.call(command, shell=True)
             time.sleep(1)
     except KeyboardInterrupt:
-        print 'Goodbye'
+        print('\nGoodbye')
+
+if __name__ == '__main__':
+    main()
